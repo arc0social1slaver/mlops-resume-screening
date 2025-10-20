e1ce782b76ab6ef5340ed09c06537f7b9813dc8a
arc0social1slaver <thanhphat2004vl@gmail.com>
2025-10-20 15:55:01 +0700

    fix: formate black bug

--- src/model/model-building.py
+++ src/model/model-building.py
@@ -9,12 +9,22 @@ def model_training(X_train, y_train) -> None:
     try:
         knn_model = OneVsRestClassifier(KNeighborsClassifier())
         knn_model.fit(X_train, y_train)
-        save_object(knn_model, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data/res/clf.pkl'))
+        save_object(
+            knn_model,
+            os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "../../data/res/clf.pkl"
+            ),
+        )
     except Exception as e:
         print(e)
         raise
 
-if __name__=="__main__":
-    train_data = load_data(data_url=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data/processed/train.csv'))
+
+if __name__ == "__main__":
+    train_data = load_data(
+        data_url=os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "../../data/processed/train.csv"
+        )
+    )
     X_train, y_train = ProcessingData(train_data).standardize_data()
     model_training(X_train, y_train)
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -6,17 +6,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_mlflow_url():
-    return os.getenv('MLFLOW_ENDPOINT_URL')
+    return os.getenv("MLFLOW_ENDPOINT_URL")
+
 
 def load_params(param_path: str) -> dict:
     try:
-        with open(param_path, 'r') as file:
+        with open(param_path, "r") as file:
             params = yaml.safe_load(file)
         return params
     except Exception as e:
         print(e)
         raise
+
 
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
@@ -27,22 +30,26 @@ def load_data(data_url: str) -> pd.DataFrame:
         print(e)
         raise
 
-def save_data(data : pd.DataFrame, data_path: str, file_name : str) -> None:
+
+def save_data(data: pd.DataFrame, data_path: str, file_name: str) -> None:
     """Load data from a CSV file."""
     try:
         os.makedirs(data_path, exist_ok=True)
-        raw_data_path = os.path.join(data_path, file_name)        
+        raw_data_path = os.path.join(data_path, file_name)
 
         data.to_csv(raw_data_path, index=False)
     except Exception as e:
         print(e)
         raise
 
-def save_train_test_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str, mode: str) -> None:
+
+def save_train_test_data(
+    train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str, mode: str
+) -> None:
     """Save the train and test datasets, creating the folder depending on mode if it doesn't exist."""
     try:
         raw_data_path = os.path.join(data_path, mode)
-        
+
         os.makedirs(raw_data_path, exist_ok=True)
 
         # Save the train and test data
@@ -53,23 +60,25 @@ def save_train_test_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data
         print(e)
         raise
 
+
 def save_object(obj, file_path: str) -> None:
     """
     Save object; e.g, model, encoder or vectorize tool to specific file path
     """
     try:
-        with open(file_path, 'wb') as file:
+        with open(file_path, "wb") as file:
             pickle.dump(obj, file)
     except Exception as e:
         print(e)
         raise
+
 
 def load_object(file_path: str):
     """
     Load object; e.g, model, encoder or vectorize tool from specific file path
     """
     try:
-        with open(file_path, 'rb') as file:
+        with open(file_path, "rb") as file:
             return pickle.load(file)
     except Exception as e:
         print(e)
