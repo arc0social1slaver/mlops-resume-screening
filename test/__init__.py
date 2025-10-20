3f099e3128c92c9e4b37a62f82bf394a3c2ceb03
arc0social1slaver <thanhphat2004vl@gmail.com>
2025-10-20 14:46:45 +0700

    test: add sample test case

---
+++ test/__init__.py
@@ -0,0 +1,19 @@
+from .util import prediction
+import unittest
+
+class Prediction(unittest.TestCase):
+
+    def test_001(self):
+        self.assertEqual(prediction("Data_Science.txt"), "Data Science")
+    
+    def test_002(self):
+        self.assertEqual(prediction("Jane_Smith.txt"), "Health and fitness")
+    
+    def test_003(self):
+        self.assertEqual(prediction("John_Doe.txt"), "Network Security Engineer")
+
+    def test_004(self):
+        self.assertEqual(prediction("Sarah_Williams.txt"), "Advocate")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/util/__init__.py
@@ -0,0 +1,31 @@
+import os
+from ui.util import cleanResume
+from src.util import load_object
+
+
+def prediction(fileName : str):
+    my_tfidf = load_object(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../artifact/tfidf.pkl'))
+    my_le = load_object(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../artifact/encoder.pkl'))
+
+    my_model = load_object(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../artifact/clf.pkl'))
+
+    # Preprocess the input text (e.g., cleaning, etc.)
+
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), f"../input/{fileName}"), 'r') as file:
+        input_resume = file.read()
+
+    cleaned_text = cleanResume(input_resume) 
+
+    # Vectorize the cleaned text using the same TF-IDF vectorizer used during training
+    vectorized_text = my_tfidf.transform([cleaned_text])
+    
+    # Convert sparse matrix to dense
+    vectorized_text = vectorized_text.toarray()
+
+    # Prediction
+    predicted_category = my_model.predict(vectorized_text)
+
+    # get name of predicted category
+    predicted_category_name = my_le.inverse_transform(predicted_category)
+
+    return predicted_category_name[0]  # Return the category name
